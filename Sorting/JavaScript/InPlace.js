3815bc405be3be2f002a129a493584976fbbba44
Chann <kingring23@hotmail.com>
2020-04-02 10:56:48 +0530

    InPlace algorithm.

---
+++ Sorting/JavaScript/InPlace.js
@@ -0,0 +1,17 @@
+/***
+ * In-place Algorithm
+ * to rever the array
+ */
+export const reverseArray = arr => {
+    if (arr && arr.constructor == Array && arr.length > 1) {
+        const arrLength = arr.length;
+        const limit = Math.ceil(arrLength / 2);
+        for (let i = 0; i < limit; i++) {
+            let temp = arr[i];
+            arr[i] = arr[arrLength - 1 - i];
+            arr[arrLength - 1 - i] = temp; 
+        }
+        return arr;
+    }
+    return arr;
+}
